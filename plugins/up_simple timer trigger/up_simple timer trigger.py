d5d3b7f90e4d1c4613cee659692d7c7adedd9a4b
Alberto Guerra <guerralberto98@gmail.com>
2020-08-22 01:37:14 +0200

    Updated time trigger, improved efficiency

--- plugins/up_simple timer trigger/up_simple timer trigger.py
+++ plugins/up_simple timer trigger/up_simple timer trigger.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from ultrasonics import logs
-from datetime import datetime, timedelta
 import json
 import os
 import io
@@ -19,7 +18,7 @@ handshake = {
         "playlists",
         "songs"
     ],
-    "version": 0.1,
+    "version": 0.2,
     "settings": [
         
     ]
@@ -28,66 +27,72 @@ handshake = {
 
 def run(settings_dict, database, component=None, songs_dict=None):
     """
-    if songs_dict is not supplied, this is an input plugin. it must return a songs_dict
-    if songs_dict is supplied, it can be a modifier (and also returns songs_dict) or an output (and does not return anything)
+    Creates a JSON file containing the date of creation of the applet/last run of the applet
+    and takes the interval time given by the user to calculate the sleep time which will
+    keep the program on hold until the next applet run
     """
-    
-    # Create a date_dict.json file if non exists and save the current date in it
-    # This only happens on creation
-    if not os.path.isfile(os.path.join(os.path.dirname(__file__), "date_dict.json")):
-        current_date = datetime.utcnow()
-        date_str = current_date.strftime("%m/%d/%Y")
-        time_str = current_date.strftime("%H:%M:%S")
-        time_zone_str = current_date.strftime("%z")
+    # bypass the trigger run if continuous runtime is selected
+    if not ("continious_runtime" in settings_dict):
+        # Create a date_dict.json file if non exists and save the current date in it
+        # This only happens on creation
+        if not os.path.isfile(os.path.join(os.path.dirname(__file__), "date_dict.json")):
 
+            # Variable containing the current timestamp in unix epoch format (seconds)
+            date_created = int(time.time())
+
+            # create a dictionary to store the creation date
+            date_dict = {
+                "date": date_created
+            }
+            # store the date in the JSON file
+            with io.open(os.path.join(os.path.dirname(__file__), "date_dict.json"), 'w') as outfile:
+                json.dump(date_dict, outfile)
+
+        # load the data stored in the JSON file
+        date_dict = json.load(io.open(os.path.join(os.path.dirname(__file__), "date_dict.json")))
+
+        # fetch the last sync date from the date_dict
+        last_sync_date = date_dict["date"]
+       
+        # load the interval time multiplier input from the settings_dict
+        try:
+            interval_multiplier = int(settings_dict["interval_input"])
+            success = True
+        except Exception as e:
+            log.error(e)
+            success = False
+            # if unsuccessful set the multiplier to 1
+        if not success:
+            log.info(f"Trigger plugin input multipler undefined, setting it to 1")
+            interval_multiplier = 1
+
+        interval_selection = settings_dict["update_frequency"]
+
+        # this dictionary translates the interval_selection to seconds
+        interval_options = {
+            "Hours": 3600,
+            "Days": 86400,
+            "Weeks": 604800,
+            "Months": 2628000
+        }
+
+        # get the interval in seconds
+        interval = interval_options[interval_selection] * interval_multiplier
+
+        # take the current date
+        current_date = int(time.time())
+
+        # calculate the sleep time from current conditions
+        sleep_time = last_sync_date + interval - current_date 
+        time.sleep(sleep_time)
+
+        # record current date and time and store it in the JSON file
+        current_date = int(time.time())
         date_dict = {
-            "date": date_str,
-            "time": time_str,
-            "time_zone": time_zone_str
+            "date": current_date
         }
         with io.open(os.path.join(os.path.dirname(__file__), "date_dict.json"), 'w') as outfile:
             json.dump(date_dict, outfile)
-
-    # load the data stored in the JSON file
-    date_dict = json.load(io.open(os.path.join(os.path.dirname(__file__), "date_dict.json")))
-    # save it into a string 
-    #last_sync_datetime_str = date_dict['date'] + "," + date_dict['time'] + date_dict['time_zone']
-    last_sync_datetime_str = (f"{date_dict['date']}, {date_dict['time']}")
-    # create a datetime object from the string
-    last_sync_datetime_obj = datetime.strptime(last_sync_datetime_str, "%m/%d/%Y, %H:%M:%S")
-
-    # load the interval time selected from the settings_dict
-    interval = settings_dict["sync_interval"]
-    interval_seconds = {
-        "Every Hour": 3600,
-        "Every Day": 86400,
-        "Every Week": 604800,
-        "Once a Month": 2628000,
-        "Once a Year": 31540000}
-    # create a timedelta object with the interval time in seconds
-    interval_obj = timedelta(seconds=interval_seconds[interval])
-
-    #create object for current time
-    current_datetime = datetime.utcnow()
-
-    # Main loop, refresh every 5 seconds
-    while ((last_sync_datetime_obj + interval_obj) > current_datetime):
-        time.sleep(5)
-        current_datetime = datetime.utcnow()
-
-    # record current date and time and store it in the JSON file
-    current_date = datetime.utcnow()
-    date_str = current_date.strftime("%m/%d/%Y")
-    time_str = current_date.strftime("%H:%M:%S")
-    time_zone_str = current_date.strftime("%z")
-
-    date_dict = {
-        "date": date_str,
-        "time": time_str,
-        "time_zone": time_zone_str
-    }
-    with io.open(os.path.join(os.path.dirname(__file__), "date_dict.json"), 'w') as outfile:
-        json.dump(date_dict, outfile)
 
 
 def builder(database, component=None):
@@ -99,20 +104,35 @@ def builder(database, component=None):
     """
 
     settings_dict = [
-        
+        {
+            "type": "string",
+            "value": "How often would you like the applet to run?"
+        },
+        {
+            "type": "checkbox",
+            "label": "Continiously",
+            "name": "continious_runtime",
+            "value": "true",
+            "id": "continious_runtime"
+        },
+        {
+            "type": "text",
+            "label": "Run every",
+            "name": "interval_input",
+            "value": "10"
+        },
         {
             "type": "select",
-            "label": "Sync time interval",
-            "name": "sync_interval",
+            "label": "",
+            "name": "update_frequency",
             "options": [
-                "Every Hour",
-                "Every Day",
-                "Every Week",
-                "Once a Month",
-                "Once a Year"
+                "Hours",
+                "Days",
+                "Weeks",
+                "Months"
             ]
-        },
-        
+        }
+     
     ]
 
     return settings_dict
