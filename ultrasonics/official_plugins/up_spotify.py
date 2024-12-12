43515a0d2a65478c6b27b943c88969676d3bde76
zodihax <parity.umpires-0m@icloud.com>
2024-12-12 09:48:52 +0100

    Handle none type (#63)

--- ultrasonics/official_plugins/up_spotify.py
+++ ultrasonics/official_plugins/up_spotify.py
@@ -555,6 +555,9 @@ def run(settings_dict, **kwargs):
             songs_dict = []
 
             for playlist in playlists:
+                if not isinstance(playlist, dict) or playlist.get("name") is None or playlist.get("id") is None:
+                    continue
+
                 item = {"name": playlist["name"], "id": {"spotify": playlist["id"]}}
 
                 songs_dict.append(item)
